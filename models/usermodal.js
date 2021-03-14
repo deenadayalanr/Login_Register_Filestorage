5d59f3c7427a878acc476f2e6d138f27520109e8
Deena1999 <deenadayalan24vvv@gmail.com>
2021-03-14 23:04:37 +0530

    Added util

---
+++ models/usermodal.js
@@ -0,0 +1,12 @@
+module.exports=class User {
+    constructor(name,email,password,mobilenumber) {
+        this.name=name;
+        this.email=email;
+        this.password=password;
+        this.mobilenumber=mobilenumber;
+    }
+
+    register() {
+        
+    }
+}
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -2,4 +2,14 @@ const express=require('express');
 
 const router=express.Router();
 
+router.post('/register',(req,res,next) => {
+    const Person={
+        name:req.body.username,
+        email:req.body.email,
+        passWord:req.body.password,
+        mobileNumber:req.body.mobilenumber,
+    };
+    console.log(Person);
+});
+
 module.exports=router;
